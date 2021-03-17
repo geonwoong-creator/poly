e1628f4707767ef23f1078337a1993f6b4322d5c
data05 <data05@DESKTOP-QQSJLE8>
2021-03-17 10:08:25 +0900

    5+2

---
+++ GwparkPrj/src/Hello22.java
@@ -0,0 +1,14 @@
+
+public class Hello22 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a, b;
+
+		a = 5;
+		b = 2;
+		System.out.println(a + b);
+
+	}
+
+}
